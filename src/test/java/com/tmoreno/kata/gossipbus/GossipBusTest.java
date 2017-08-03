6bff4e3a61a8a703b9df088c1d371f6eb7bc99e0
tmoreno <tmorenobernal@yahoo.es>
2017-08-03 13:11:16 +0200

    Added test when drivers join at last stop

--- src/test/java/com/tmoreno/kata/gossipbus/GossipBusTest.java
+++ src/test/java/com/tmoreno/kata/gossipbus/GossipBusTest.java
@@ -10,6 +10,7 @@ public class GossipBusTest {
 
 	private static final Integer[] MIKE_ROUTE = { 1, 2, 3, 4 };
 	private static final Integer[] PETER_ROUTE = { 1, 4, 5, 6 };
+	private static final Integer[] BILL_ROUTE = { 2, 3, 5, 4 };
 
 	@Before
 	public void setUp() {
@@ -34,4 +35,15 @@ public class GossipBusTest {
 
 		Assert.assertEquals("1", numStops);
 	}
+
+	@Test
+	public void whenTwoDriversJoinAtLastStopReturnRouteLength() {
+		gossipBus.addDriverRoute(MIKE_ROUTE);
+		gossipBus.addDriverRoute(BILL_ROUTE);
+
+		String numStops = gossipBus.calcNumStops();
+
+		Assert.assertEquals("4", numStops);
+	}
+
 }
