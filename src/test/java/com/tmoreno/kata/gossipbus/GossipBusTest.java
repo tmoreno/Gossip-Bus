231b10486755983584f7ba7a1086696ea3c5c61c
tmoreno <tmorenobernal@yahoo.es>
2017-08-03 12:29:02 +0200

    Added addDriverRoute method

--- src/test/java/com/tmoreno/kata/gossipbus/GossipBusTest.java
+++ src/test/java/com/tmoreno/kata/gossipbus/GossipBusTest.java
@@ -6,13 +6,28 @@ import org.junit.Test;
 public class GossipBusTest {
 
 	private static final int[] MIKE_ROUTE = { 1, 2, 3, 4 };
+	private static final int[] PETER_ROUTE = { 1, 4, 5, 6 };
 
 	@Test
 	public void whenOneDriverReturnNever() {
 		GossipBus gossipBus = new GossipBus();
 
-		String numStops = gossipBus.calcNumStops(MIKE_ROUTE);
+		gossipBus.addDriverRoute(MIKE_ROUTE);
+
+		String numStops = gossipBus.calcNumStops();
 
 		Assert.assertEquals("never", numStops);
+	}
+
+	@Test
+	public void whenTwoDriversSameStart() {
+		GossipBus gossipBus = new GossipBus();
+
+		gossipBus.addDriverRoute(MIKE_ROUTE);
+		gossipBus.addDriverRoute(PETER_ROUTE);
+
+		String numStops = gossipBus.calcNumStops();
+
+		Assert.assertEquals("1", numStops);
 	}
 }
