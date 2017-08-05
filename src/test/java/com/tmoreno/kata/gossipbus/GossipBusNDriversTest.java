1126449b37516b5cf1886b89e7acdf0e7a12406e
tmoreno <tmorenobernal@yahoo.es>
2017-08-05 12:21:13 +0200

    Changed to use Driver class

--- src/test/java/com/tmoreno/kata/gossipbus/GossipBusNDriversTest.java
+++ src/test/java/com/tmoreno/kata/gossipbus/GossipBusNDriversTest.java
@@ -7,28 +7,39 @@ import org.junit.Test;
 public class GossipBusNDriversTest {
 
 	private String numStops;
-	private Routes routes;
+	private Driver mike;
+	private Driver peter;
+	private Driver bill;
+	private Driver james;
+	private Driver colin;
+	private Driver laura;
 	private GossipBus gossipBus;
 
-	private static final Integer[] MIKE_ROUTE = { 1, 2, 3, 4 };
-	private static final Integer[] PETER_ROUTE = { 1, 4, 5, 6 };
-	private static final Integer[] BILL_ROUTE = { 2, 3, 6, 4 };
-	private static final Integer[] JAMES_ROUTE = { 2, 3, 4, 5, 1 };
-	private static final Integer[] COLIN_ROUTES = { 1, 3, 4, 5 };
-	private static final Integer[] LAURA_ROUTES = { 3, 2, 1, 4 };
+	private static final int[] MIKE_ROUTE = { 1, 2, 3, 4 };
+	private static final int[] PETER_ROUTE = { 1, 4, 5, 6 };
+	private static final int[] BILL_ROUTE = { 2, 3, 6, 4 };
+	private static final int[] JAMES_ROUTE = { 2, 3, 4, 5, 1 };
+	private static final int[] COLIN_ROUTES = { 1, 3, 4, 5 };
+	private static final int[] LAURA_ROUTES = { 3, 2, 1, 4 };
 
 	@Before
 	public void setUp() {
-		routes = new Routes();
+		mike = new Driver(MIKE_ROUTE);
+		peter = new Driver(PETER_ROUTE);
+		bill = new Driver(BILL_ROUTE);
+		james = new Driver(JAMES_ROUTE);
+		colin = new Driver(COLIN_ROUTES);
+		laura = new Driver(LAURA_ROUTES);
+
+		gossipBus = new GossipBus();
 	}
 
 	@Test
 	public void whenAllDriversAreAtFirstStopReturnOne() {
-		routes.addDriverRoute(MIKE_ROUTE);
-		routes.addDriverRoute(PETER_ROUTE);
-		routes.addDriverRoute(COLIN_ROUTES);
+		gossipBus.addDriver(mike);
+		gossipBus.addDriver(peter);
+		gossipBus.addDriver(colin);
 
-		gossipBus = new GossipBus(routes);
 		numStops = gossipBus.calcNumStops();
 
 		Assert.assertEquals("1", numStops);
@@ -36,11 +47,10 @@ public class GossipBusNDriversTest {
 
 	@Test
 	public void whenAllDriversAreAtLastStopReturnRouteLength() {
-		routes.addDriverRoute(MIKE_ROUTE);
-		routes.addDriverRoute(BILL_ROUTE);
-		routes.addDriverRoute(LAURA_ROUTES);
+		gossipBus.addDriver(mike);
+		gossipBus.addDriver(bill);
+		gossipBus.addDriver(laura);
 
-		gossipBus = new GossipBus(routes);
 		numStops = gossipBus.calcNumStops();
 
 		Assert.assertEquals("4", numStops);
@@ -48,11 +58,10 @@ public class GossipBusNDriversTest {
 
 	@Test
 	public void whenDriversJoinAfterALoopLongestRouteLength() {
-		routes.addDriverRoute(MIKE_ROUTE);
-		routes.addDriverRoute(PETER_ROUTE);
-		routes.addDriverRoute(JAMES_ROUTE);
+		gossipBus.addDriver(mike);
+		gossipBus.addDriver(peter);
+		gossipBus.addDriver(james);
 
-		gossipBus = new GossipBus(routes);
 		numStops = gossipBus.calcNumStops();
 
 		Assert.assertEquals("5", numStops);
@@ -60,15 +69,18 @@ public class GossipBusNDriversTest {
 
 	@Test
 	public void kataExample1() {
-		Integer[] driver1 = { 3, 1, 2, 3 };
-		Integer[] driver2 = { 3, 2, 3, 1 };
-		Integer[] driver3 = { 4, 2, 3, 4, 5 };
+		int[] driver1Route = { 3, 1, 2, 3 };
+		Driver driver1 = new Driver(driver1Route);
+		gossipBus.addDriver(driver1);
 
-		routes.addDriverRoute(driver1);
-		routes.addDriverRoute(driver2);
-		routes.addDriverRoute(driver3);
+		int[] driver2Route = { 3, 2, 3, 1 };
+		Driver driver2 = new Driver(driver2Route);
+		gossipBus.addDriver(driver2);
 
-		gossipBus = new GossipBus(routes);
+		int[] driver3Route = { 4, 2, 3, 4, 5 };
+		Driver driver3 = new Driver(driver3Route);
+		gossipBus.addDriver(driver3);
+
 		numStops = gossipBus.calcNumStops();
 
 		Assert.assertEquals("5", numStops);
